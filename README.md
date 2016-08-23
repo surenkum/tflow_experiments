901054390c0b8fc64e38513b7dd7019f8dabfc6b
Fnu Suren Kumar <surenkum@zonule.eecs.umich.edu>
2016-08-23 17:47:52 -0400

    Added linear regression code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tflow_experiments
 My experiments with tensor flow
+
+# Linear Regression
+- Based on https://cs224d.stanford.edu/lectures/CS224d-Lecture7.pdf
+
+- You can visualize the variables and resulting loss functions over iterations by
+using tensorboard
+
+- Initial values really matter. If your initial guess if way off, reduce the
+  learning rate and increase the number of iterations 
---
+++ linear_regression.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pdb
+import numpy as np
+
+if __name__ == "__main__":
+    # Create a dummy data for linear regression
+    x_data = np.arange(10,step=0.01)
+    y_data = 2*x_data+10#+np.sin(x_data/10)
+    #plt.scatter(x_data,y_data)
+    #plt.show()
+
+    nsamples = 1000
+    nbatch  = 100
+    X_data = np.reshape(x_data,(nsamples,1))
+    Y_data = np.reshape(y_data,(nsamples,1))
+
+    # Creating placeholders for input
+    X = tf.placeholder(tf.float32,shape=(nbatch,1))
+    y = tf.placeholder(tf.float32,shape=(nbatch,1))
+    sess = tf.InteractiveSession()
+    # Create everything within a variable scope
+    with tf.variable_scope("linear-regression"):
+        W = tf.get_variable("weights",(1,1),initializer =\
+                tf.random_normal_initializer())
+        b = tf.get_variable("bias",(1,),initializer = \
+                tf.constant_initializer(3.0))
+        y_pred = tf.add(tf.matmul(X,W),b)
+        tf.histogram_summary('pred',y_pred)
+        loss = tf.reduce_sum((y-y_pred)**2/nbatch)
+        # Create an optimizer
+        opt = tf.train.AdamOptimizer().minimize(loss)
+        tf.scalar_summary('loss',loss)
+        tf.scalar_summary('weight',W[0,0])
+        tf.scalar_summary('bias',b[0])
+        merged = tf.merge_all_summaries()
+        train_writer = tf.train.SummaryWriter('train/',
+                                      sess.graph)
+
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            for i in range(100000):
+                indices = np.random.choice(nsamples,nbatch)
+                X_batch,y_batch = X_data[indices],Y_data[indices]
+                # Do gradient descent
+                _,loss_val,summary = sess.run([opt,loss,merged],\
+                        feed_dict = {X:X_batch,y:y_batch})
+                train_writer.add_summary(summary,i)
